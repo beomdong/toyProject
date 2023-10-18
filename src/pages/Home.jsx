f6a5d0bd6f5f480fd9117725ba0c560e7b5e1648
LBD <ehdclrltk12@naver.com>
2023-10-18 17:23:21 +0900

    make shape

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Banner from '../components/Banner'
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      홈
+    </div>
+  );
+}
+
+export default Home
