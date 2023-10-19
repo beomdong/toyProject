2a741e0decbaa354574f58ad30db2a116448f623
LBD <ehdclrltk12@naver.com>
2023-10-19 10:03:32 +0900

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      헤더
-    </div>
+    <nav>
+      <h1>
+        <img src="./img/title.png" alt="" />
+      </h1>
+      <ul>
+        <li>회사 소개</li>
+        <li> 서비스</li>
+        <li> 함께가치</li>
+        <li> 뉴스룸</li>
+      </ul>
+      <div>
+        <span>로그인</span> | <span>회원가입</span>
+      </div>
+    </nav>
   )
 }
 
